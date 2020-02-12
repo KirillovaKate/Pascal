e4c030e4093763ba99db60c80711a82966d9e48d
Qvocha <pineappletoe@gmail.com>
2020-02-13 00:04:36 +0300

    hh

---
+++ 12022020/1.pas
@@ -0,0 +1,21 @@
+﻿const
+  n = 5;
+
+begin
+  var a: array[1..n] of integer := (3, 4, 1, 2, 5);
+  
+  for var step := 1 to n - 1 do
+  begin
+    var max: integer := integer.MinValue;
+    var indexOfMax: integer := -1;
+    for var i := step to n do
+      if a[i] > max then
+      begin
+        max := a[i];
+        indexOfMax := i;
+      end;
+    swap(a[step], a[indexOfMax]);
+  end;
+  
+  write(a);
+end.
---
+++ 12022020/2.1.pas
@@ -0,0 +1,11 @@
+﻿const
+  n = 6;
+  
+begin
+  var a: array[1..n] of integer := (5, 2, -1, 3, -2, 0);
+  for var step := 1 to n do
+    for var i := 1 to n - 1 do
+      if (a[i] >= 0) and (a[i + 1] < 0) then
+        swap(a[i], a[i + 1]);
+  write(a);
+end.
