e38ce1557bd3e3f5c6f3f81a0e7fb15948056491
Qvocha <pineappletoe@gmail.com>
2020-02-26 18:37:50 +0300

    v2 begin

---
+++ 26022020/v2/4.pas
@@ -0,0 +1,40 @@
+﻿const
+  m = 4;
+  n = 5;
+  
+begin
+  var y: array[1..m, 1..n] of integer;
+  for var i := 1 to m do
+  begin
+    for var j := 1 to n do
+    begin
+      y[i, j] := Random(10, 20);
+      write(y[i, j], ' ');
+    end;
+    writeln;
+  end;
+  var max := 0;
+  var min := m;
+  var indexOfMax := -1;
+  var indexOfMin := -1;
+  for var i := 1 to m do
+  begin
+    var k := 0;
+    for var j := 1 to n do
+      if y[i, j] mod 2 = 0 then
+        k += 1;
+    if k < min then
+    begin
+      min := k;
+      indexOfMin := i;
+    end;
+    if k > max then
+    begin
+      max := k;
+      indexOfMax := i;
+    end;
+  end;
+  Writeln;
+  Writeln('min: ', indexOfMin);
+  Writeln('max: ', indexOfMax);
+end.
