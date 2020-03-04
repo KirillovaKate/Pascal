8cf2d2dd2413aa4c0202aaca072ebfc70b6d9a99
Qvocha <pineappletoe@gmail.com>
2020-03-04 17:57:06 +0300

    lol

---
+++ 04032020/1.pas
@@ -0,0 +1,19 @@
+﻿const
+  n = 10;
+
+begin
+  var k: integer;
+  var a: array[1..n] of integer := (1, 2, 3, 44, 54, 64, 7, 8, 94, 10);
+  Writeln(a);
+  var b: array[1..n] of integer;
+  Read(k);
+  var j := 0;
+  for var i := 1 to n do
+    if a[i] mod 10 = k then
+    begin
+      j += 1;
+      b[j] := a[i];
+    end;
+  for var i := 1 to j do
+    Write(b[i], ' ');
+end.
---
+++ 04032020/2.pas
@@ -0,0 +1,24 @@
+﻿const
+  n = 4;
+  
+begin
+  var x: array[1..n] of real := (1, 2, 3, 4);
+  var a: array[1..n, 1..n] of real;
+  Writeln(x);
+  for var j := 1 to n do
+  begin
+    var y := 1.0;
+    for var i := 1 to n do
+    begin
+      a[i, j] := y;
+      y *= x[j];
+    end;
+  end;
+  
+  for var i := 1 to n do
+  begin
+    for var j := 1 to n do
+      Write(a[i, j], ' ');
+    writeln;
+  end;
+end.
