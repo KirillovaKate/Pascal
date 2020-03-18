909669322b3f19631108a0a83d5677e2bd6881fb
Qvocha <pineappletoe@gmail.com>
2020-03-18 17:30:49 +0300

    inf

---
+++ 26022020/v2/1.pas
@@ -0,0 +1,22 @@
+﻿begin
+  var a: array[1..15] of integer;
+  for var i := 1 to 15 do
+    a[i] := random(-10, 10);
+  writeln(a);
+  var s := 0;
+  var max := -11;
+  var indexOfMax := -1;
+  for var i := 1 to 15 do
+  begin
+    if abs(a[i]) mod 10 = 3 then
+      s += a[i];
+    if (i mod 2 = 1) and (a[i] > max) then
+    begin
+      max := a[i];
+      indexOfMax := i;
+    end;
+  end;
+  swap(a[1], a[indexOfMax]);
+  writeln(s);
+  writeln(a);
+end.
---
+++ 26022020/v2/2.pas
@@ -0,0 +1,14 @@
+﻿const
+  n = 10;
+
+begin
+  var a: array[1..n] of integer;
+  for var i := 1 to n do
+    a[i] := random(-10, 10);
+  writeln(a);
+  var temp := a[1];
+  for var i := 1 to n - 1 do
+    a[i] := a[i + 1];
+  a[n] := temp;
+  write(a);
+end.
